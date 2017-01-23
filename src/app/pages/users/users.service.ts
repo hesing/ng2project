3079f6247afd4dff208aaf845fdcfdd24ee01626
Hemant <hesing@microsoft.com>
2017-01-23 23:27:33 +0530

    format user data

--- src/app/pages/users/users.service.ts
+++ src/app/pages/users/users.service.ts
@@ -9,16 +9,28 @@ export class UsersService {
 	private usersUrl:string = 'https://reqres.in/api/users';
 	constructor(private http: Http) {}
 
+	// map user to desired format
+	private mapUser(user): User{
+		return {
+			id: user.id,
+			name: `${user.first_name} ${user.last_name}`,
+			username: user.first_name,
+			avatar: user.avatar
+		};
+	}
+
 	// Get all users
 	getUsers(): Observable<User[]>{
 		return this.http.get(this.usersUrl)
-				.map(res => res.json().data);
+				.map(res => res.json().data)
+				.map(users => users.map(this.mapUser));
 	}
 
 	// Get single user
 	getUser(id): Observable<User>{
 		return this.http.get(`${this.usersUrl}/${id}`)
-				.map(res => res.json().data);
+				.map(res => res.json().data)
+				.map(this.mapUser);
 	}
 
 	// Create a user

0e9a0b2040b61f7a7b7eb5a41de8b1d4775d1888
Hemant <hesing@microsoft.com>
2017-01-28 15:54:54 +0530

    MDF - collect form data with FormGroup

--- src/app/pages/mdf/mdf.component.ts
+++ src/app/pages/mdf/mdf.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-mdf',
   templateUrl: './mdf.component.html',
   styleUrls: ['./mdf.component.scss']
 })
 export class MdfComponent implements OnInit {
+  userForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+  	this.userForm = new FormGroup({
+  		name: new FormControl(''),
+  		username: new FormControl('')
+  	});
   }
 
+  createUser(){
+  	console.log(this.userForm.value);
+  }
 }
--- src/app/pages/mdf/mdf.module.ts
+++ src/app/pages/mdf/mdf.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MdfRoutingModule } from './mdf-routing.module';
 import { MdfComponent } from './mdf.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    MdfRoutingModule
+    MdfRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [MdfComponent]
 })

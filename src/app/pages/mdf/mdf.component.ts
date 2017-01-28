f6c1d2dfa061eed458f58a506c5db03d3844b2ad
Hemant <hesing@microsoft.com>
2017-01-28 18:46:42 +0530

    MDF  form validation cleanup

--- src/app/pages/mdf/mdf.component.ts
+++ src/app/pages/mdf/mdf.component.ts
@@ -2,44 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-mdf',
-  templateUrl: './mdf.component.html',
-  styleUrls: ['./mdf.component.scss']
+	selector: 'app-mdf',
+	templateUrl: './mdf.component.html',
+	styleUrls: ['./mdf.component.scss']
 })
 export class MdfComponent implements OnInit {
-  userForm: FormGroup;
-  nameError: string = '';
-  usernameError: string = '';
+	userForm: FormGroup;
+	nameError: string = '';
+	usernameError: string = '';
 
-  constructor(private fb: FormBuilder) { }
+	constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-  	this.userForm = this.fb.group({
-  		name: ['', [ Validators.minLength(3), Validators.maxLength(6) ]],
-  		username: ['']
-  	});
+	validateForm() {
+		this.nameError = '';
+		this.usernameError = '';
+		let name = this.userForm.get('name');
+		let username = this.userForm.get('username');
 
-  	this.userForm.valueChanges
-  		.subscribe(res => {
-  			this.nameError = '';
-  			this.usernameError = '';
-  			let name = this.userForm.get('name');
-  			let username = this.userForm.get('username');
+		if (name.dirty && name.invalid) {
+			if (name.errors['required']) this.nameError = 'Name is required.';
+			if (name.errors['minlength']) this.nameError = 'Name must be at least 3 characters.';
+			if (name.errors['maxlength']) this.nameError = 'Name can\'t be more than 6 characters.';
+		}
 
-  			if(name.dirty && name.invalid){
-			    if (name.errors['required'])  this.nameError = 'Name is required.';
-				if (name.errors['minlength']) this.nameError = 'Name must be at least 3 characters.';
-				if (name.errors['maxlength']) this.nameError = 'Name can\'t be more than 6 characters.';
-			}
+		if (username.dirty && username.invalid) {
+			this.usernameError = 'Username is required';
+		}
+	}
 
-  			if(username.dirty && username.invalid){
-  				this.usernameError = 'Username is required';
-  			}
+	buildForm() {
+		this.userForm = this.fb.group({
+			name: ['', [Validators.minLength(3), Validators.maxLength(6)]],
+			username: ['']
+		});
 
-  		});
-  }
+		this.userForm.valueChanges
+			.subscribe(res => this.validateForm());
+	}
 
-  createUser(){
-  	console.log(this.userForm.value);
-  }
+	ngOnInit() {
+		this.buildForm();
+	}
+
+	createUser() {
+		console.log(this.userForm.value);
+	}
 }
